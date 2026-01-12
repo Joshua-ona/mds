19414022d42d66a622bf5e1828144f21bba5f8e9
Joshua-ona <joshkendrickona@gmail.com>
2026-01-12 22:37:58 +0300

    Update JSX content

--- mdh/src/Gallery.jsx
+++ mdh/src/Gallery.jsx
@@ -11,20 +11,20 @@ function Gallery() {
       <div className="gallery-grid">
                 
           <div className="gallery-card" >
-            <img className="gallery-card"  src="images (5).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="download (4).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="images (6).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="images (3).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="images (1).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="images (2).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="images (7).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="exercise.jpeg" alt="praying"></img>
-            <img  className="gallery-card"  src="child.jpeg" alt="praying"></img>
-            <img className="gallery-card"  src="prayer.jpeg" alt="praying"></img>
-            <img  className="gallery-card"  src="destiny.webp" alt="praying"></img>
-            <img className="gallery-card"  src="download.jpg" alt="praying"></img>
-            <img className="gallery-card"  src="download (1).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="Naume.jpg" alt="praying"></img>
+            <img className="gallery-card"  src="/images (5).jpg" alt="praying"></img>
+            <img className="gallery-card"  src="/download (4).jpg" alt="praying"></img>
+            <img className="gallery-card"  src="/images (6).jpg" alt="praying"></img>
+            <img className="gallery-card"  src="/images (3).jpg" alt="praying"></img>
+            <img className="gallery-card"  src="/images (1).jpg" alt="praying"></img>
+            <img className="gallery-card"  src="/images (2).jpg" alt="praying"></img>
+            <img className="gallery-card"  src="/images (7).jpg" alt="praying"></img>
+            <img className="gallery-card"  src="/exercise.jpeg" alt="praying"></img>
+            <img  className="gallery-card"  src="/child.jpeg" alt="praying"></img>
+            <img className="gallery-card"  src="/prayer.jpeg" alt="praying"></img>
+            <img  className="gallery-card"  src="/destiny.webp" alt="praying"></img>
+            <img className="gallery-card"  src="/download.jpg" alt="praying"></img>
+            <img className="gallery-card"  src="/download (1).jpg" alt="praying"></img>
+            <img className="gallery-card"  src="/Naume.jpg" alt="praying"></img>
           
           </div>
       
