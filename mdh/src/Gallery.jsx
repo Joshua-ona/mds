19b3a8bb89be001e7acf3e720f1a6e03140d3c69
Joshua-ona <joshkendrickona@gmail.com>
2026-01-18 05:18:56 +0300

    Normalize image filenames: remove spaces/parentheses and standardize case

--- mdh/src/Gallery.jsx
+++ mdh/src/Gallery.jsx
@@ -1,35 +1,42 @@
 import React from "react";
 import "./Gallery.css";
-import image from "./assets/images (5).jpg";
 
+// import assets from src (rename files to avoid spaces/parentheses)
+import img1 from "./assets/images-5.jpg";
+import img2 from "./assets/download-4.jpg";
+import img3 from "./assets/images-6.jpg";
+import img4 from "./assets/images-3.jpg";
+import img5 from "./assets/images-1.jpg";
+import img6 from "./assets/images-2.jpg";
+import img7 from "./assets/images-7.jpg";
+import exercise from "./assets/exercise.jpeg";
+import child from "./assets/child.jpeg";
+import prayer from "./assets/prayer.jpeg";
+import destiny from "./assets/destiny.webp";
+import download from "./assets/download.jpg";
+import download1 from "./assets/download-1.jpg";
+import naume from "./assets/Naume.jpg";
 
+const images = [
+  img1, img2, img3, img4, img5, img6, img7,
+  exercise, child, prayer, destiny, download, download1, naume
+];
 
 function Gallery() {
   return (
     <div className="gallery-container">
       <h2 className="gallery-title">Our School Gallery</h2>
-
       <div className="gallery-grid">
-                
-          <div className="gallery-card" >
-            <img className="gallery-card"  src={image} alt="praying"></img>
-            <img className="gallery-card"  src="./download (4).JPG" alt="praying"></img>
-            <img className="gallery-card"  src="/images (6).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="/images (3).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="/images (1).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="/images (2).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="/images (7).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="/exercise.jpeg" alt="praying"></img>
-            <img  className="gallery-card"  src="/child.jpeg" alt="praying"></img>
-            <img className="gallery-card"  src="/prayer.jpeg" alt="praying"></img>
-            <img  className="gallery-card"  src="/destiny.webp" alt="praying"></img>
-            <img className="gallery-card"  src="/download.jpg" alt="praying"></img>
-            <img className="gallery-card"  src="/download (1).jpg" alt="praying"></img>
-            <img className="gallery-card"  src="./Naume.jpg" alt="praying"></img>
-             <img src={process.env.PUBLIC_URL + '/images/Naume.jpg'} alt="hello" />
-          
-          </div>
-      
+        <div className="gallery-card">
+          {images.map((src, i) => (
+            <img
+              key={i}
+              className="gallery-image"
+              src={src}
+              alt={`gallery-${i}`}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
